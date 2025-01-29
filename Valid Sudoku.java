07e531f505db7bb2e810d03a9df9117be8e18f22
Srikari Obulshetty <srikariobulshetty@gmail.com>
2025-01-29 19:37:51 +0530

    Create Valid Sudoku.java

---
+++ Valid Sudoku.java
@@ -0,0 +1,45 @@
+class Solution {
+    public static boolean checkRow(char [][]board, int r, int c){
+           for(int j=0;j<9;j++){
+               if(j!=c && board[r][c]==board[r][j]){
+                  return false;
+               }
+           }
+           return true;
+    }
+
+    public static boolean checkCol(char [][]board, int r, int c){
+         for(int i=0;i<9;i++){
+             if(i!=r && board[r][c]==board[i][c]){
+                 return false;
+             }
+         }
+         return true;
+    }
+
+    public static boolean checkBox(char [][]board, int r, int c){
+          int st_row=(r/3)*3;
+          int st_col=(c/3)*3;
+          for(int i=st_row;i<st_row+3;i++){
+                for(int j=st_col;j<st_col+3;j++){
+                     if(i!=r && j!=c && board[i][j]==board[r][c]){
+                          return false;
+                     }
+                }
+          }
+          return true;
+
+    }
+    public boolean isValidSudoku(char[][] board) {
+         for(int i=0;i<9;i++){
+             for(int j=0;j<9;j++){
+                 if(board[i][j]!='.'){
+                      if( !checkRow(board,i,j)) return false;
+                      if(!checkCol(board, i, j)) return false;
+                      if( !checkBox(board, i, j)) return false;
+                 }
+             }
+         }
+         return true;
+    }
+}
